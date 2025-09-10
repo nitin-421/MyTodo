2c0d46edf6321524c63dc2fdc34d44cd8dbe1e0a
nitin-421 <myselfmaurya@outlook.com>
2025-09-11 00:29:31 +0530

    VS-Git

--- server/database/db.js
+++ server/database/db.js
@@ -1,40 +1,13 @@
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const USERNAME = process.env.DB_USERNAME;
-// const PASSWORD = process.env.DB_PASSWORD;
-
-// const Connection = () => {
-//   const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.omgzz0f.mongodb.net/?retryWrites=true&w=majority`;
-
-//   mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-//   mongoose.connection.on("connected", () => {
-//     console.log("Database connected Successfully");
-//   });
-
-//   mongoose.connection.on("disconnected", () => {
-//     console.log("Database disconnected");
-//   });
-
-//   mongoose.connection.on("error", (error) => {
-//     console.log("Error while connecting with the database ", error.message);
-//   });
-// };
-
-// export default Connection;
-
-//connect to mongodb database function es6 using import
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD;
+// const USERNAME = process.env.DB_USERNAME;
+// const PASSWORD = process.env.DB_PASSWORD;
 
-const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.omgzz0f.mongodb.net/?retryWrites=true&w=majority`;
+// const MONGODB_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.omgzz0f.mongodb.net/?retryWrites=true&w=majority`;
+
+const MONGODB_URI = process.env.MONGO_URI;
 
 /**
  * This function connects to a MongoDB database using the provided URI and logs a message indicating
